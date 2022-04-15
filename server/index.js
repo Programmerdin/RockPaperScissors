462279cb5e9831112bd3c1d33df8364054e834ff
Programmerdin <testingemailodin@gmail.com>
2022-04-15 09:50:27 +0900

    before restructuring

--- server/index.js
+++ server/index.js
@@ -5,6 +5,9 @@ const {Server} = require('socket.io')
 const cors = require("cors");
 const { isPromise } = require('util/types');
 const { compileFunction } = require('vm');
+const {GAME_NOT_READY, GAME_READY, GAME_START, GAME_END} = require('./message_meaning');
+
+
 
 app.use(cors());
 
@@ -21,12 +24,12 @@ const io = new Server(server, {
 
 
 let global_room = [
-    {roomId: "testroom1",
-    last_move_list: [{"testconnection1": 0},{"testconnection2": 0}],
-    game_state: "not ready"
+    {
+        roomId: "testroom1",
+        last_move_list: [{"testconnection1": 0},{"testconnection2": 0}],
+        game_state: GAME_NOT_READY
     }
 ];
-
 
 io.on("connection",(socket)=>{
     console.log(`user connected: ${socket.id}`);
@@ -44,7 +47,7 @@ io.on("connection",(socket)=>{
                 {
                     roomId: data.roomId, 
                     last_move_list: [{[data.id]: 0}], 
-                    game_state: "not ready"
+                    game_state: GAME_NOT_READY
                 }
             );
             //redefine room_index so that it is no longer -1
@@ -52,8 +55,7 @@ io.on("connection",(socket)=>{
         //if room1 alreayd exists then need to find if the particular client has already joined    
         //this global_room[room_index].last_move_list.findIndex(x=>x.id === data.id) is not working
         //idk why
-        } else if(global_room[room_index].last_move_list.findIndex(x=>x.id === data.id) === -1){
-            console.log(global_room[room_index].last_move_list.findIndex(x=>x.id === data.id));
+        } else if(global_room[room_index].last_move_list.findIndex(x=>data.id in x) === -1){
             //if room1 exists in globalroom and this client hasn't joined already 
             global_room[room_index].last_move_list.push({[data.id]: 0});
         } //if room1 already exists and client has already joined, no need to do anything
@@ -63,29 +65,48 @@ io.on("connection",(socket)=>{
         let number_of_players_in_room = global_room[room_index].last_move_list.length;
         if(number_of_players_in_room >= 2){
             //let clients know game is ready to start
-            io.in(data.room).emit("receive_message", "game is ready to start");
+            io.in(data.room).emit("receive_message", GAME_READY);
             //update game_state
-            global_room[room_index].game_state = "ready";
+            global_room[room_index].game_state = GAME_READY;
             console.log("room "+data.roomId+" is ready to start")
-        }
 
-        console.log(global_room[room_index].last_move_list);
+            console.log("game has started");
+            global_room[room_index].game_state = GAME_START;
+            io.in(data.room).emit("receive_message", GAME_START);
+
+            //wait 10 seconds then
+            setTimeout(function(){
+                //update game state to GAME_END
+                global_room[room_index].game_state = GAME_END;
+                //tell clients game is done 
+                io.in(data.room).emit("receive_message", GAME_END);
+                
+                //tell each client their respective opponent's move
+                if (global_room[room_index].last_move_list.findIndex(x=> data.id in x) === 0){
+                    io.to(data.id).emit("receive_message", global_room[room_index].last_move_list[1].) 
+                    
+                }
+                
+            }, 10000);
+        }
 
     })
 
     socket.on("send_message", (data)=>{
         console.log(data.id+" played "+data.player_move);
         io.to(data.id).emit("receive_message", data.player_move);
+        //if game state is GAME_START then update player moves if not player move won't be updated
+        let current_global_room = global_room.find(x=>x.roomId); 
+        if (current_global_room.game_state === GAME_START){
+            current_global_room.game_state = data.player_move;
+        }
+
+
     });
 
 
 
-
-
-
 });
-
-
 
 
 
---
+++ server/message_meaning.js
@@ -0,0 +1,7 @@
+const GAME_NOT_READY = 0;
+const GAME_READY = 1;
+const GAME_START = 2;
+const GAME_END = 3;
+
+
+module.exports = {GAME_NOT_READY, GAME_READY, GAME_START, GAME_END};
